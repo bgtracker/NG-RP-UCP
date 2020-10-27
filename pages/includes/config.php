0b90d13dc44cdc75c8c7b4713da62f4834f63630
Pavel <littleham1996@gmail.com>
2020-10-27 10:18:04 +0200

    Add files via upload

--- pages/includes/config.php
+++ pages/includes/config.php
@@ -1,7 +1,23 @@
 <?php
-$con = new PDO("mysql:host=localhost;dbname=db", "user", "password");
+$con = new PDO("mysql:host=localhost;dbname=samp", "root", "");
 	
 session_start();
+
+$expireAfter = 15; // 15 Minutes
+
+	if(isset($_SESSION['last_action'])){
+
+	    $secondsInactive = time() - $_SESSION['last_action'];
+	    $expireAfterSeconds = $expireAfter * 60; 
+	    
+	    if($secondsInactive >= $expireAfterSeconds) {
+
+	        session_unset();
+	        session_destroy();
+	        header("Location: login.php");
+	    }
+	}
+
 function checkForLogin()
 {
 	if(!isset($_SESSION['playername']))
